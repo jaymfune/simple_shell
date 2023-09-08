8f59752f76cf7244c1c96cf7cc70824df6b92a9b
jaymfune <jacobmfune2@gmail.com>
2023-09-08 10:04:08 -0700

    simple shell implement tokenization of commands and handle exit command

--- main.c
+++ main.c
@@ -1,18 +1,61 @@
 #include "main.h"
 
-int main(int ac, char **argv)
+#define MAX_ARGS 10
+
+int main()
 {
 	char *prompt = "($)"; /* Prompt to print to terminal */
 
 	/* To store the address of the buffer holding the user input */
 	char *user_input; 
 	size_t n = 0; /* to store alloacted size in bytes */
+	ssize_t input_length;
+	char* args[MAX_ARGS];
+	int i;
 
-	(void)ac; (void)argv;
+	while (1)
+	{
+		printf("%s", prompt);
+		input_length  = getline(&user_input, &n, stdin);
 
-	printf("%s", prompt);
-	getline(&user_input, &n, stdin);
-	printf("%s", user_input);
+		/* Handle Ctrl+D (end-of-file condition) or other input errors */
+		if (input_length  == -1)
+		{
+			printf("Exiting shell....\n");
+			free(user_input);
+			return(-1);
+		}
+		else
+		{
+			/**
+			  * Break the user input into separate commands
+			  * to execute the commands
+			  */
+			i = 0;
+			args[i] = strtok(user_input, " \n");
+
+			while (args[i] != NULL)
+			{
+				printf("Token: %s\n", args[i]);
+				i++;
+
+				/* get next the next string */
+				args[i] = strtok(NULL, " \n");
+			}
+			args[i] = NULL;
+
+			/* Handle the exit built-in command */
+			if (strcmp(args[0], "exit") == 0)
+			{
+				printf("Exiting shell....\n");
+				return (0);
+			}
+		}
+
+		printf("%s", user_input);
+	}
+
+	free(user_input);
 
 	return (0);
 }
