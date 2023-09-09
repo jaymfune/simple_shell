fc4ecae24a5853fba21b2b56c5364e2bb79017cc
jaymfune <jacobmfune2@gmail.com>
2023-09-09 03:09:42 -0700

    simple shell implement execution of commands

--- main.c
+++ main.c
@@ -12,11 +12,14 @@ int main()
 	ssize_t input_length;
 	char* args[MAX_ARGS];
 	int i;
+	pid_t pid;
 
 	while (1)
 	{
 		printf("%s", prompt);
 		input_length  = getline(&user_input, &n, stdin);
+
+		/* printf("%ld", input_length); */
 
 		/* Handle Ctrl+D (end-of-file condition) or other input errors */
 		if (input_length  == -1)
@@ -50,7 +53,33 @@ int main()
 				printf("Exiting shell....\n");
 				return (0);
 			}
+
+			/*Execute the command */
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("Fork failed");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{
+				if (execvp(args[0], args) == -1)
+				{
+					perror("Command not found");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				int status;
+				if (waitpid(pid, &status, 0) == -1)
+				{
+					perror("Waitpid failed");
+					exit(EXIT_FAILURE);
+				}
+			}
 		}
+		
 
 		printf("%s", user_input);
 	}
